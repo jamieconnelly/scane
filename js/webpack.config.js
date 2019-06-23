18fe7514d8db6290449f6d13b8dbac24b0dc9a1e
jamieconnelly <jamiebruceconnelly@gmail.com>
2019-06-23 22:03:32 +0200

    :art: clean webpack.config.js

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -3,70 +3,84 @@ const path = require('path')
 
 const jsRootDir = path.resolve(__dirname) // You are here
 const repoRootDir = path.resolve(jsRootDir, '../')
-const outputDir = path.resolve(repoRootDir, 'src/scane/dj/static/build')
+const buildDir = path.resolve(repoRootDir, 'src/scane/dj/static/build')
+const templateDir = path.resolve(repoRootDir, 'src/scane/dj/templates')
 
 const isDevelopment = true
 
-module.exports = {
-  entry: { index: './src/index.tsx' },
-  plugins: [
+const getOutput = () => {
+  return {
+    filename: 'bundle.js',
+    path: buildDir,
+  }
+}
+
+const getPlugins = () => {
+  return [
     new MiniCssExtractPlugin({
       filename: isDevelopment ? '[name].css' : '[name].[hash].css',
       chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css',
     }),
-  ],
+  ]
+}
+
+const getRules = () => {
+  return [
+    { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+    // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+    { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
+    {
+      test: /\.module\.s(a|c)ss$/,
+      loader: [
+        isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+            localIdentName: '[name]__[local]___[hash:base64:5]',
+            camelCase: true,
+            sourceMap: isDevelopment,
+          },
+        },
+        {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: isDevelopment,
+          },
+        },
+      ],
+    },
+    {
+      test: /\.s(a|c)ss$/,
+      exclude: /\.module.(s(a|c)ss)$/,
+      loader: [
+        isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+        'css-loader',
+        {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: isDevelopment,
+          },
+        },
+      ],
+    },
+  ]
+}
+
+module.exports = {
+  entry: `${jsRootDir}/src/index.tsx`,
+  output: getOutput(),
+  plugins: getPlugins(),
   module: {
-    rules: [
-      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-      { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
-      {
-        test: /\.module\.s(a|c)ss$/,
-        loader: [
-          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              localIdentName: '[name]__[local]___[hash:base64:5]',
-              camelCase: true,
-              sourceMap: isDevelopment,
-            },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: isDevelopment,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.s(a|c)ss$/,
-        exclude: /\.module.(s(a|c)ss)$/,
-        loader: [
-          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: isDevelopment,
-            },
-          },
-        ],
-      },
-    ],
+    rules: getRules(),
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js', '.scss'],
   },
-  output: {
-    filename: 'bundle.js',
-    publicPath: outputDir + '/',
-    path: outputDir,
-    globalObject: 'this',
+  devServer: {
+    hot: true,
+    inline: true,
+    port: 3001,
+    contentBase: 'src',
   },
-  devtool: 'inline-source-map',
-  mode: 'development',
 }

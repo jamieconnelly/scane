636f27a63a8f5b8a4a9272aa89a84a9d4d87e93b
jamieconnelly <jamiebruceconnelly@gmail.com>
2019-06-25 21:36:56 +0200

    :sparkles: LoginForm: use final-form

--- js/src/App.tsx
+++ js/src/App.tsx
@@ -1,70 +1,12 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { graphql } from 'react-relay'
 
 import withQueryRenderer, { OuterProps } from './relay/withQueryRenderer'
 
 import { AppQuery } from './__generated__/AppQuery.graphql'
-import login from './mutations'
+import LoginForm from './LoginForm'
 
 interface IProps extends OuterProps<AppQuery, {}> {}
-
-const LoginForm = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-
-  const onSetUsername = (evt: React.SyntheticEvent) => {
-    const target = evt.target as HTMLInputElement
-    setUsername(target.value)
-  }
-  const onSetPassword = (evt: React.SyntheticEvent) => {
-    const target = evt.target as HTMLInputElement
-    setPassword(target.value)
-  }
-
-  const [userOrError, setUserOrError] = useState()
-
-  const onSubmit = async () => {
-    try {
-      const { userLogin } = await login({ password, username })
-      const name = userLogin && userLogin.user && userLogin.user.firstName
-      setUserOrError(`Hello ${name} 👋🏻`)
-    } catch (e) {
-      if (e instanceof Array) {
-        setUserOrError(e.map(({ message }) => message).join(', '))
-      } else {
-        setUserOrError(e.message)
-      }
-    }
-  }
-
-  return (
-    <div>
-      Login Form
-      <div>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          required
-          name="username"
-          value={username}
-          onChange={onSetUsername}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          required
-          name="password"
-          value={password}
-          onChange={onSetPassword}
-        />
-        <button onClick={onSubmit}>Login</button>
-      </div>
-      {userOrError}
-    </div>
-  )
-}
 
 const App = (props: IProps) => {
   if (props.relayLoading) {
---
+++ js/src/LoginForm.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { Form, Field } from 'react-final-form'
+
+import { LoginMutationInput } from './__generated__/mutationsLoginMutation.graphql'
+import login from './mutations'
+
+const LoginForm = () => {
+  const [userOrError, setUserOrError] = useState()
+
+  const onSubmit = async (values: LoginMutationInput) => {
+    try {
+      const { userLogin } = await login(values)
+      const name = userLogin && userLogin.user && userLogin.user.firstName
+      setUserOrError(`Hello ${name} 👋🏻`)
+    } catch (e) {
+      if (e instanceof Array) {
+        setUserOrError(e.map(({ message }) => message).join(', '))
+      } else {
+        setUserOrError(e.message)
+      }
+    }
+  }
+
+  return (
+    <>
+      <Form
+        onSubmit={onSubmit}
+        render={({ handleSubmit, submitting, pristine }) => (
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label>Username</label>
+              <Field name="username" component="input" type="text" required />
+            </div>
+            <div>
+              <label>Password</label>
+              <Field name="password" component="input" type="password" required />
+            </div>
+            <div>
+              <button type="submit" disabled={submitting || pristine}>
+                Login
+              </button>
+            </div>
+          </form>
+        )}
+      />
+      {userOrError}
+    </>
+  )
+}
+
+export default LoginForm

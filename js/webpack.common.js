da4f3275a19674b2422b6a8d3af3165b20753f15
jamieconnelly <jamiebruceconnelly@gmail.com>
2019-06-27 00:02:06 +0200

    :heavy_minus_sign: Remove source-map-loader

--- js/webpack.common.js
+++ js/webpack.common.js
@@ -11,8 +11,6 @@ module.exports = {
   devtool: 'source-map',
   module: {
     rules: [
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
       {
         exclude: [/node_modules/],
         test: /\.(j|t)sx?$/,

2b32bedbca76041dc348de5658b5c8a52f8becd8
jamieconnelly <jamiebruceconnelly@gmail.com>
2019-06-30 21:42:44 +0200

    :wrench: Use postgres db

--- src/scane/settings.py
+++ src/scane/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 WEBPACK_OUTPUT_DIR = os.path.join(BASE_DIR, 'scane/dj/static/build')
@@ -77,12 +79,8 @@ WSGI_APPLICATION = 'scane.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.parse(os.getenv('DATABASE_URL'))
 
 
 # Password validation

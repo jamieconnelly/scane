162a390cc2f676135071123fa9e3c96dc5995196
jamieconnelly <jamiebruceconnelly@gmail.com>
2019-06-30 20:46:27 +0200

    :wrench: Add scane.herokuapp.com as an allowed host

--- src/scane/settings.py
+++ src/scane/settings.py
@@ -112,6 +112,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+ALLOWED_HOSTS = ['scane.herokuapp.com']
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/

acf5b046695117666f6bc7c5737d6c08c11415a8
jamieconnelly <jamiebruceconnelly@gmail.com>
2019-07-01 19:46:28 +0200

    :bug: Fix settings paths

--- src/scane/settings/base.py
+++ src/scane/settings/base.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-WEBPACK_OUTPUT_DIR = os.path.join(BASE_DIR, 'scane/dj/static/build')
+WEBPACK_OUTPUT_DIR = os.path.join(BASE_DIR, 'dj/static/build')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -102,5 +102,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'scane/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (WEBPACK_OUTPUT_DIR,)

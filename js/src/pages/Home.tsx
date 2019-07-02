9682bc92d40760193b9da8a14bbd0c66fe4755ad
jamieconnelly <jamiebruceconnelly@gmail.com>
2019-07-02 21:01:00 +0200

    :sparkles: Create basic SideBar layout

--- js/src/pages/Home.tsx
+++ js/src/pages/Home.tsx
@@ -1,5 +1,25 @@
 import React from 'react'
 
-const Home = () => <div>Welcome 🍾</div>
+import styles from './Home.scss'
+
+const Sidebar = () => (
+  <div className={styles.sideBar}>
+    <div className={styles.sideBarHeader}>Project Name</div>
+    <div className={styles.linksContainer}>
+      <ul>
+        <li>SERP Scrape</li>
+        <li>Pull Backlinks</li>
+        <li>Get Contact Info</li>
+        <li>Mail Campaign</li>
+      </ul>
+    </div>
+  </div>
+)
+
+const Home = () => (
+  <div>
+    <Sidebar />
+  </div>
+)
 
 export default Home

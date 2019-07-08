197bd202b85cb76680179ae2f3ddfadf107b7a34
jamieconnelly <jamiebruceconnelly@gmail.com>
2019-07-08 20:25:18 +0200

    :wrench: Fix production logging config

--- src/scane/settings/production.py
+++ src/scane/settings/production.py
@@ -15,23 +15,21 @@ USE_HEADLESS_CHROME = True
 CHROME_DOWNLOADS_DIR = '/tmp/downloads'
 EXPORTS_DIR = '/tmp/exports'
 
-LOGGING = (
-    {
-        'version': 1,
-        'disable_existing_loggers': False,
-        'formatters': {
-            'console': {'format': '%(name)-12s %(levelname)-8s %(message)s'},
-            'file': {'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'},
-        },
-        'handlers': {
-            'console': {'class': 'logging.StreamHandler', 'formatter': 'console'},
-            'file': {
-                'level': 'INFO',
-                'class': 'logging.FileHandler',
-                'formatter': 'file',
-                'filename': '/tmp/debug.log',
-            },
-        },
-        'loggers': {'': {'level': 'INFO', 'handlers': ['console', 'file']}},
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console': {'format': '%(name)-12s %(levelname)-8s %(message)s'},
+        'file': {'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'},
     },
-)
+    'handlers': {
+        'console': {'class': 'logging.StreamHandler', 'formatter': 'console'},
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'formatter': 'file',
+            'filename': '/tmp/debug.log',
+        },
+    },
+    'loggers': {'': {'level': 'INFO', 'handlers': ['console', 'file']}},
+}

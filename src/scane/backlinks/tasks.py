b2c1425190e2fa884a5d0d46b35aac1dc7947266
jamieconnelly <jamiebruceconnelly@gmail.com>
2019-07-13 12:43:34 +0200

    :art: Add max_retries

--- src/scane/backlinks/tasks.py
+++ src/scane/backlinks/tasks.py
@@ -3,6 +3,12 @@ from celery import task
 from . import backlinks as backlinks_helpers
 
 
-@task(acks_late=True, reject_on_worker_lost=True)
+@task(
+    autoretry_for=(Exception,),
+    default_retry_delay=60,
+    max_retries=5,
+    acks_late=True,
+    reject_on_worker_lost=True,
+)
 def get_backlinks(data):
     backlinks_helpers.get_backlinks(data)
